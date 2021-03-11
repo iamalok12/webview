f2bc11a87ce4a1266fff3ddcc24494b7bebd91a1
Alok <ialok@outlook.com>
2021-03-11 23:09:44 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:io';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Text("Webview Implemented",style: TextStyle(fontSize: 30),),
+              Expanded(
+                child: WebView(
+                  initialUrl: 'https://keysq8.com',
+                ),
+              ),
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
